ff640423978face179bb586ae3f262abb30b24f1
Thomas Sommavilla <thomas.sommavilla.@washout-app.com>
2021-05-18 17:36:32 +0200

    feat: add TODO tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// TODO
+
+test("creates 8 folded cards starting by 4 pokemons", () => {});
+
+test("turnes clicked card", () => {});
+
+test("turnes max 2 cards", () => {});
+
+test("checks if 2 cards match", () => {
+  test("folds cards if they don't match", () => {});
+
+  test("doesn't fold cards if they match", () => {});
+});
+
+test("checks if all cards are matched", () => {
+  test("shows 'YOU WIN' component if all cards match", () => {});
 });
